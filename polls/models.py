f8dace7d935d5742a982adf1aa6ce17e601e272f
Shashwot Adhikari <shashwot123@gmail.com>
2025-05-17 14:07:00 +0545

    Changed how the Admin Site looks

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
@@ -9,6 +10,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    
+    @admin.display(
+            boolean = True,
+            ordering = pub_date,
+            description = "Published Recently?"
+    )
     
     def was_published_recently(self): 
         return timezone.now() - datetime.timedelta(days = 1) <= self.pub_date <= timezone.now()
